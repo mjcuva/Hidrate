74c4adf596a1018f89aa73c66fc72cbd92097923
Marc <mjcuva@gmail.com>
2014-09-13 15:20:11 -0500

    Validation

--- Hidrate/Controllers/HiInitialSetupViewController.m
+++ Hidrate/Controllers/HiInitialSetupViewController.m
@@ -30,10 +30,35 @@
 - (void)viewDidLoad
 {
     [[self navigationItem] setHidesBackButton:YES];
+    self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"full-gradient-background.png"]];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
 }
 
+- (void)keyboardWillShow:(NSNotification *)notification{
+    [self.tableView setContentOffset:CGPointMake(0, 80) animated:YES];
+}
+
+- (NSTimeInterval)keyboardAnimationDurationForNotification:(NSNotification*)notification
+{
+    NSDictionary* info = [notification userInfo];
+    NSValue* value = [info objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval duration = 0;
+    [value getValue:&duration];
+    return duration;
+}
+
+
 - (IBAction)continue {
-    NSLog(@"HERE");
+    int age = [self.enterAge.text intValue];
+    double weight = [self.enterWeight.text doubleValue];
+    int feet = [self.enterFeet.text intValue];
+    int inches = [self.enterInches.text intValue];
+    if(age == 0 || weight == 0 || feet == 0 || inches == 0){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Missing Data" message:@"You must complete all the fields" delegate:self cancelButtonTitle:nil otherButtonTitles:@"Ok", nil];
+        [alert show];
+    }else{
+        [self performSegueWithIdentifier:@"showToday" sender:self];
+    }
 }
 
 @end
