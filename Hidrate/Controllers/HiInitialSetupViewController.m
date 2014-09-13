bbbe6b532e40c78dbd19705942d8d0f98923d3f5
Matthew Lewis <matt@mplewis.com>
2014-09-13 10:27:26 -0500

    Set up basic screen flow

--- Hidrate/Controllers/HiInitialSetupViewController.m
+++ Hidrate/Controllers/HiInitialSetupViewController.m
@@ -26,7 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[self navigationItem] setHidesBackButton:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,16 +38,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
