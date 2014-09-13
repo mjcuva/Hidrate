b9c5cabe465c8a12e5d4cfdde4fe05acfcd90104
Matthew Lewis <matt@mplewis.com>
2014-09-13 10:13:00 -0500

    Add new VCs

---
+++ Hidrate/Controllers/HiTodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  HiTodayViewController.h
+//  Hidrate
+//
+//  Created by Matthew Lewis on 9/13/14.
+//  Copyright (c) 2014 Hidrate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HiTodayViewController : UIViewController
+
+@end
---
+++ Hidrate/Controllers/HiWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HiWelcomeViewController.h
+//  Hidrate
+//
+//  Created by Matthew Lewis on 9/13/14.
+//  Copyright (c) 2014 Hidrate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HiWelcomeViewController : UIViewController
+
+@end
